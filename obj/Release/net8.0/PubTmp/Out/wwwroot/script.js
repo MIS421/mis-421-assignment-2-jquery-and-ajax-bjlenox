a2fb2aac86c659794169263b4739004979c0245f
Ben <bjlenox@crimson.ua.edu>
2024-02-15 14:20:46 -0600

    final

--- obj/Release/net8.0/PubTmp/Out/wwwroot/script.js
+++ obj/Release/net8.0/PubTmp/Out/wwwroot/script.js
@@ -2,6 +2,7 @@ var len;
 var results = '';
 
 function apiSearch() {
+    document.getElementById("searchResults").style = "visibility: visible;";
   var params = {
     "q": $("#query").val(),
     "count": "50",
@@ -23,7 +24,13 @@ function apiSearch() {
       }
 
       $('#searchResults').html(results);
-      $('#searchResults').dialog();
+        $('#searchResults').dialog({
+            title: 'Search results:',
+            height: 500,
+            width: 1000
+            
+
+        });
     })
     .fail(function () {
       alert("error");
@@ -32,16 +39,39 @@ function apiSearch() {
 
 function whatTime() {
     console.log("time called");
+    document.getElementById("time").style = "visibility: visible;";
     var now = new Date();
     var hours = now.getHours();
+    if (hours > 12) {
+        hours = hours - 12;
+    }
     var mins = now.getMinutes();
-    var time = hours + ":" + minutes;
+    if (mins < 10) {
+        var time = hours + ":0" + mins;
+    }
+    else {
+        var time = hours + ":" + mins;
+    }
+    var time = hours + ":" + mins;
     document.getElementById('time').innerHTML = time;
+    $('#time').html(time);
     $('#time').dialog({
-        title: 'Current time'
+        title: 'Current time',
+        
     });
 }
+var count = 0;
 function changeImg() {
     console.log("img changing");
-    document.body.style.backgroundImage = "url(https://images.unsplash.com/photo-1540573133985-87b6da6d54a9?q=80&w=1776&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)"
+    if (count == 0) {
+
+        console.log("new");
+        document.body.style.backgroundImage = "url(https://images.unsplash.com/photo-1540573133985-87b6da6d54a9?q=80&w=1776&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)"
+        count = 1;
+    }
+    else {
+        console.log("old");
+        document.body.style.backgroundImage = "url(https://images.unsplash.com/photo-1463852247062-1bbca38f7805?q=80&w=1776&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)"
+        count = 0;
+    }
 }
--- obj/Release/net8.0/WebApplication1.AssemblyInfo.cs
+++ obj/Release/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7295fa73ead9b51c4dca221e94878e4f8792b6a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1446707938321419d8a95e94658ff5d4cce6d24b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
